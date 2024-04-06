0e0d8e64292340497154240b704d56d405692f20
Ivan Lin <ilin@g.clemson.edu>
2024-04-05 22:40:49 -0400

    added util file

---
+++ services/util.py
@@ -0,0 +1,19 @@
+import os
+from typing import Union
+from pathlib import Path
+
+
+class Util:
+    """
+    Miscellaneous Functions
+    """
+    @staticmethod
+    def list_files_in_directory(directory: Union[Path, str]):
+        """
+        @return: list of all files in directory
+        """
+        return [
+            os.path.join(root, filename)
+            for root, directories, files in os.walk(directory)
+            for filename in files
+        ]
