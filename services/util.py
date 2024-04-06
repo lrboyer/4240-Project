6829256b2832e32da674cadb364394eed21325db
Ivan Lin <ilin@g.clemson.edu>
2024-04-05 23:09:56 -0400

    updated docstring format

--- services/util.py
+++ services/util.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def list_files_in_directory(directory: Union[Path, str]):
     """
-    @return: list of all files in directory
+    :return: list of all files in directory
     """
     return [
         os.path.join(root, filename)
