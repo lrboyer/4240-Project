729aa94019f7e943f1ce2ad675e0d96d7986f8f4
Ivan Lin <ilin@g.clemson.edu>
2024-04-05 22:52:10 -0400

    added to db index to config

--- services/config.py
+++ services/config.py
@@ -1,1 +1,2 @@
+MALWARE_DB_INDEX_NAME = "sha256_hash"
 MALWARE_DB_FILE = "./full_sha256.db"
--- services/database_reader.py
+++ services/database_reader.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from typing import Union
 import pandas as pd
+import config
 
 
 class DatabaseReader:
     @staticmethod
     def read_database(path: Union[Path, str]):
         db = pd.read_csv(path, comment='#')
-        db.set_index("sha256_hash", inplace=True)
+        db.set_index(config.MALWARE_DB_INDEX_NAME, inplace=True)
         return db
