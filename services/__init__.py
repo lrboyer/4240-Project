89d7d042dc3e7f47c02d0bcd5ee8ce31473f111f
Ivan Lin <ilin@g.clemson.edu>
2024-04-05 21:34:37 -0400

    renamed folder and added __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,5 @@
+from services.stopwatch import Stopwatch
+from services.database_reader import DatabaseReader
+from services.malwarescanner import MalwareScanner
+from services import config
+from services.args import Args
