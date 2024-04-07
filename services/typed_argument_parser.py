5e5786c01d909f5dd35e98eccbf5266444e22189
Ivan Lin <ilin@g.clemson.edu>
2024-04-07 15:01:19 -0400

    updated arguments

--- services/typed_argument_parser.py
+++ services/typed_argument_parser.py
@@ -1,4 +1,5 @@
 from tap import Tap
+from typing import Optional
 
 
 class TypedArgumentParser(Tap):
@@ -7,6 +8,8 @@ class TypedArgumentParser(Tap):
     It does stuff
     """
     directory_to_scan: str  # directory to scan
+    verbose: bool = False
 
     def configure(self) -> None:
         self.add_argument('directory_to_scan')
+        self.add_argument('-v', '--verbose')
