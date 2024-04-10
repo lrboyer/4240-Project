0f1c701beb240474be7a8e3f48b88eaa5d1b24fe
Ivan Lin <ilin@g.clemson.edu>
2024-04-09 22:07:21 -0400

    refactored hash_file

--- services/util.py
+++ services/util.py
@@ -1,4 +1,6 @@
 import os
+import hashlib
+import sys
 from typing import Union
 from pathlib import Path
 
@@ -12,3 +14,16 @@ def list_files_in_directory(directory: Union[Path, str]):
         for root, directories, files in os.walk(directory)
         for filename in files
     ]
+
+
+def hash_file(file_name: str):
+    try:
+        if not (os.path.isfile(file_name)):
+            return
+        with open(file_name, 'rb') as f:
+            file_name_read = f.read()
+            file_hash = hashlib.sha256(file_name_read).hexdigest()
+            return file_hash
+
+    except Exception as e:
+        print(e, file=sys.stderr)
